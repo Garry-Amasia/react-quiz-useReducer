417096d4d0fc4b2863a4117032a9eb3cdd112759
GarryAmasia <garryamasia@outlook.com>
2023-11-13 12:21:57 +1100

    adding header component

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header className="app-header">
+      <img src="logo512.png" alt="react-logo" />
+      <h1> React Quiz</h1>
+    </header>
+  );
+};
