fb1033cc00640fa04fc09c681b49ea13c630de1f
GarryAmasia <amasiagarry87@gmail.com>
2023-11-16 19:28:45 +1100

    test

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export const Options = ({ question, dispatch, answer }) => {
   console.log(answer);
+  console.log(answer);
   return (
     <div className="options">
       {question.options.map((singleAnswer, index) => {
